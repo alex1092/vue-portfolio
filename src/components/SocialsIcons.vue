0b7c920ac3c9ab28627fafaa26b986ff89b1e589
Alex Little <alex.little@neds.com>
2024-03-28 18:48:26 +0700

    yeah

--- src/components/SocialsIcons.vue
+++ src/components/SocialsIcons.vue
@@ -1,23 +1,23 @@
 <script setup lang="ts">
-import { Linkedin } from 'lucide-vue-next'
-import { Github } from 'lucide-vue-next'
-import { Twitter } from 'lucide-vue-next'
+import { Linkedin } from "lucide-vue-next"
+import { Github } from "lucide-vue-next"
+import { Twitter } from "lucide-vue-next"
 
-import { config } from '@/config'
+import { config } from "@/config"
 </script>
 
 <template>
   <div class="flex gap-4 mt-2">
     <a v-if="config.socials.github" :href="config.socials.github" target="_blank">
-      <Github class="w-6 h-6 hover:text-gray-500" />
+      <Github class="w-6 h-6 opacity-60 hover:opacity-100" />
     </a>
     <div v-else></div>
     <a v-if="config.socials.linkedin" :href="config.socials.linkedin" target="_blank">
-      <Linkedin class="w-6 h-6 hover:text-gray-500" />
+      <Linkedin class="w-6 h-6 opacity-60 hover:opacity-100" />
     </a>
     <div v-else></div>
     <a v-if="config.socials.twitter" :href="config.socials.twitter" target="_blank">
-      <Twitter class="w-6 h-6 hover:text-gray-500" />
+      <Twitter class="w-6 h-6 opacity-60 hover:opacity-100" />
     </a>
     <div v-else></div>
   </div>
