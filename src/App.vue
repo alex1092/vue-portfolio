4fb3e424d7bd3ab83a6f42f104f8b3dcd54f1d88
Alex Little <alex.little@neds.com>
2024-03-28 18:47:36 +0700

    yeah

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import { useMobileDetection } from '@lib/utils'
-import NavigationDrawer from './components/navigation/NavigationDrawer.vue'
-import ColorToggle from './components/ColorToggle.vue'
+import { RouterLink, RouterView } from "vue-router"
+import { useMobileDetection } from "@lib/utils"
+import NavigationDrawer from "./components/navigation/NavigationDrawer.vue"
+import ColorToggle from "./components/ColorToggle.vue"
 
 const { isMobile } = useMobileDetection()
 </script>
 
 <template>
-  <header class="absolute top-0 left-0 w-full h-16">
+  <header class="sticky top-0 left-0 w-full h-16">
     <div v-if="!isMobile" class="m-3">
       <nav class="flex justify-center items-center gap-10">
         <RouterLink to="/">Home</RouterLink>
